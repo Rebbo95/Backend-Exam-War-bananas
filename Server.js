dccd86828c06df98e866facd5a59525280df4ca4
rebbo095@gmail.com <Rebbo095@gmail.com>
2025-05-29 11:05:42 +0200

    Setup

---
+++ Server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+const routes = require('./Routes');
+
+const ConnectDb = require('./Config/ConnectDb');
+
+const app = express();
+
+app.use(express.json());
+app.use('/api', routes);
+
+
+ConnectDb();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
